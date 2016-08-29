ad7f9d2a21922d3811e05840510856d3c5987b1f
M@trix <subhashgehlot@gmail.com>
2016-08-29 22:40:36 +0530

    Task 2

--- subhashgehlot/Program.cs
+++ subhashgehlot/Program.cs
@@ -13,7 +13,7 @@ namespace subhashgehlot
 
             switch (args[0])
             {
-                //Adds the Numbers specified in the arguments
+                //Adds the Numbers specified in the arguments : Task 1
                 case "sum":
                     if (args.Length > 1)
                     { prog.Sum(args[1]); }
@@ -24,6 +24,17 @@ namespace subhashgehlot
                     }
                     break;
 
+                //Adds the Numbers specified in the arguments : Task 2
+                case "add":
+                    if (args.Length > 1)
+                    { prog.Sum(args[1]); }
+                    else
+                    {
+                        Console.Write("Sum of the numbers is : 0");
+                        Console.Read();
+                    }
+                    break;
+                    
             }
         }
 
@@ -49,5 +60,28 @@ namespace subhashgehlot
 
             }
         }
+
+        //Adds the Numbers specified in the arguments
+        void Add(string numbers)
+        {
+            int sum = 0;
+
+            if (numbers != null)
+            {
+
+                char splitter = ',';
+                string[] numbersArray = numbers.Split(splitter);
+
+                for (int counter = 0; counter < numbersArray.Count(); counter++)
+                {
+                    sum += Convert.ToInt16(numbersArray[counter]);
+                }
+
+                Console.Write("Sum of the numbers is :" + sum);
+                Console.Read();
+
+            }
+        }
+
     }
 }
